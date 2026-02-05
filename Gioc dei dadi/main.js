546cc1b06a8c5edf1297f409019f1a90dd468ed3
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-05 15:13:25 +0100

    complete gioco dei dadi

---
+++ Gioc dei dadi/main.js
@@ -0,0 +1,15 @@
+//per prima cosa dobbiamo verifiacre che il pc ci un numero random che arriva fino a 100
+// poi  facciamo la stessa cosa con lutente overro ci dia un numero randm fino a 100
+// poi merttiamo in confronto i due nummeri e il numero piu alto vince
+
+let NumberUtente = Math.floor(Math.random() * 101);
+let NumberPc = Math.floor(Math.random() * 101);
+console.log(NumberUtente, NumberPc);
+
+if(NumberUtente < NumberPc){
+    console.log("Hai il numero piu grande 'HAI VINTO'");
+    
+}else {
+    console.log("Il Pc ha il numero piu grande 'HAI PERSO'");
+    
+}
--- main.js
+++ main.js
@@ -4,19 +4,16 @@
 //se e presente nella lista email stampiomo un messagio appropriato 
 //altrimenti stampiomo un messaggio appropriato
 
-const EmailList = ["Paperino@gmail.com","Qiyana@gmail.com","Nonsonounwebdevelopment@gmail.com","Fabio@gmail.com"]
-
+let EmailList = ["Qiyana@gmail.com","Paperino@gmail.com","Nonsonounwebdevelopment@gmail.com","Fabio@gmail.com"]
 let utente = prompt("Inserisci la tua email")
 
 for (let i = 0; i < EmailList.length; i++){
     if (EmailList[i] === utente){
         console.log("Fai parte della lista Puoi entrare");
-        break;
+       
     }else{
         console.log("Non fai parte della lista non puoi entrare");
-        
+         break;
     }
 }
-
-
 
